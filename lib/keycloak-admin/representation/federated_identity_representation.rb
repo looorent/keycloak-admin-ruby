e1c54845fabc9781828a4c634b503249e95ee18e
Roman Hargrave <roman@hargrave.info>
2020-03-27 15:42:48 -0500

    Create representation for federated identities and add to user

---
+++ lib/keycloak-admin/representation/federated_identity_representation.rb
@@ -0,0 +1,15 @@
+module KeycloakAdmin
+  class FederatedIdentityRepresentation < Representation
+    attr_accessor :identity_provider,
+      :user_id,
+      :user_name
+
+    def self.from_hash(hash)
+      rep                   = new
+      rep.identity_provider = hash['identityProvider']
+      rep.user_id           = hash['userId']
+      rep.user_name         = hash['userName']
+      rep
+    end
+  end
+end
--- lib/keycloak-admin/representation/user_representation.rb
+++ lib/keycloak-admin/representation/user_representation.rb
@@ -10,27 +10,34 @@ module KeycloakAdmin
       :email_verified,
       :first_name,
       :last_name,
-      :credentials
+      :credentials,
+      :federated_identities
 
     def self.from_hash(hash)
-      user                   = new
-      user.id                = hash["id"]
-      user.created_timestamp = hash["createdTimestamp"]
-      user.origin            = hash["origin"]
-      user.username          = hash["username"]
-      user.email             = hash["email"]
-      user.enabled           = hash["enabled"]
-      user.email_verified    = hash["emailVerified"]
-      user.first_name        = hash["firstName"]
-      user.last_name         = hash["lastName"]
-      user.attributes        = hash["attributes"]
-      user.credentials       = hash["credentials"]&.map{ |hash| CredentialRepresentation.from_hash(hash) } || []
+      user                      = new
+      user.id                   = hash["id"]
+      user.created_timestamp    = hash["createdTimestamp"]
+      user.origin               = hash["origin"]
+      user.username             = hash["username"]
+      user.email                = hash["email"]
+      user.enabled              = hash["enabled"]
+      user.email_verified       = hash["emailVerified"]
+      user.first_name           = hash["firstName"]
+      user.last_name            = hash["lastName"]
+      user.attributes           = hash["attributes"]
+      user.credentials          = hash["credentials"]&.map{ |hash| CredentialRepresentation.from_hash(hash) } || []
+      user.federated_identities = hash["federatedIdentities"]&.map { |hash| FederatedIdentityRepresentation.from_hash(hash) } || []
       user
     end
 
     def add_credential(credential_representation)
       @credentials ||= []
       @credentials.push(credential_representation)
+    end
+
+    def add_federated_identity(federated_identity_representation)
+      @federated_identities ||= []
+      @federated_identities.push(federated_identity_representation)
     end
   end
 end
