e16df8c293f8589fe1822b9aed2f7c4b40d9a738
ATeal <arteal89@gmail.com>
2021-07-20 12:58:51 -0400

    Add browser_security_headers to realm representation

--- lib/keycloak-admin/representation/realm_representation.rb
+++ lib/keycloak-admin/representation/realm_representation.rb
@@ -1,13 +1,15 @@
 module KeycloakAdmin
   class RealmRepresentation < Representation
     attr_accessor :id,
-      :realm
+      :realm,
+      :browser_security_headers
     # TODO: Add more attributes
 
     def self.from_hash(hash)
       realm       = new
       realm.id    = hash["id"]
       realm.realm = hash["realm"]
+      realm.browser_security_headers = hash["browserSecurityHeaders"]
       realm
     end
   end
