669ab3f1138a053a190082268478d8391346c874
Lionnel Patrick DOOKO <patrick.lionnel@finetechgroupe.com>
2022-11-17 14:18:56 +0000

    try to revoke access_token for logout

--- lib/keycloak-admin/client/token_client.rb
+++ lib/keycloak-admin/client/token_client.rb
@@ -12,6 +12,10 @@ module KeycloakAdmin
       "#{realm_url}/protocol/openid-connect/token"
     end
 
+    def revoke_token_url(user_id)
+      "#{realm_url}/users/#{ user_id}/logout"
+    end
+
     def realm_url
       @realm_client.realm_url
     end
@@ -19,6 +23,16 @@ module KeycloakAdmin
     def get
       response = execute_http do
         RestClient::Resource.new(token_url, @configuration.rest_client_options).post(
+          @configuration.body_for_token_retrieval,
+          @configuration.headers_for_token_retrieval
+        )
+      end
+      TokenRepresentation.from_json(response.body)
+    end
+
+    def revoke(user_id) 
+      response = execute_http do
+        RestClient::Resource.new(revoke_token_url(user_id), @configuration.rest_client_options).post(
           @configuration.body_for_token_retrieval,
           @configuration.headers_for_token_retrieval
         )
