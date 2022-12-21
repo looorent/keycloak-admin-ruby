0bcb85352890c6737f9b218ed7f444b3055b46bd
Philippe Hässig <phil@nine.ch>
2022-12-21 14:58:24 +0100

    Parse subGroups for GroupRepresentation
    Implements #16

--- lib/keycloak-admin/representation/group_representation.rb
+++ lib/keycloak-admin/representation/group_representation.rb
@@ -2,13 +2,15 @@ module KeycloakAdmin
   class GroupRepresentation < Representation
     attr_accessor :id,
       :name,
-      :path
+      :path,
+      :sub_groups
 
     def self.from_hash(hash)
-      group      = new
-      group.id   = hash["id"]
-      group.name = hash["name"]
-      group.path = hash["path"]
+      group            = new
+      group.id         = hash["id"]
+      group.name       = hash["name"]
+      group.path       = hash["path"]
+      group.sub_groups = hash.fetch("subGroups", []).map { |sub_group_hash| self.from_hash(sub_group_hash) }
       group
     end
   end
