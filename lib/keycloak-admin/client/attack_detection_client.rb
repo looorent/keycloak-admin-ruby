5f2d40972be24ab7aaa4abf27fce6840725f24a4
Sanjay Prajapati <prsanjay@users.noreply.github.com>
2023-05-30 01:28:47 +0530

    Added few more APIs (#25)
    * Add redirect_uri param to execute_actions API
    
    * Add client_id
    
    * Add APIs to unlock user(s) and fetch the attack status of user
    
    * Add API to fetch active session details
    
    * Add API to fetch active session details
    
    * Add changes to update user. The previous implementation was throwing bad request error
    
    * Add API to logout user
    
    * Initialize attack detections client
    
    * Add Specs for the changes in user client and representation
    
    ---------
    
    Co-authored-by: Sanjay Prajapati <sprajapati@zeidlerlegalservices.com>

---
+++ lib/keycloak-admin/client/attack_detection_client.rb
@@ -0,0 +1,42 @@
+module KeycloakAdmin
+  class AttackDetectionClient < Client
+    def initialize(configuration, realm_client)
+      super(configuration)
+      raise ArgumentError.new("realm must be defined") unless realm_client.name_defined?
+      @realm_client = realm_client
+    end
+
+    def lock_status(user_id)
+      raise ArgumentError.new('user_id must be defined') if user_id.nil?
+
+      response = execute_http do
+        RestClient::Resource.new(brute_force_url(user_id), @configuration.rest_client_options).get(headers)
+      end
+      AttackDetectionRepresentation.from_hash(JSON.parse(response))
+    end
+
+    def unlock_user(user_id)
+      raise ArgumentError.new('user_id must be defined') if user_id.nil?
+
+      execute_http do
+        RestClient::Resource.new(brute_force_url(user_id), @configuration.rest_client_options).delete(headers)
+      end
+      true
+    end
+
+    def unlock_users
+      execute_http do
+        RestClient::Resource.new(brute_force_url, @configuration.rest_client_options).delete(headers)
+      end
+      true
+    end
+
+    def brute_force_url(user_id = nil)
+      if user_id
+        "#{@realm_client.realm_admin_url}/attack-detection/brute-force/users/#{user_id}"
+      else
+        "#{@realm_client.realm_admin_url}/attack-detection/brute-force/users"
+      end
+    end
+  end
+end
