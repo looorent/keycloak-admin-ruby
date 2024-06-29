497f074d081df0c55c268795b69e6ebb422f9952
Mohammed O. Tillawy <tillawy@gmail.com>
2024-06-29 14:45:10 +0300

    test authz_policy find_by

--- spec/client/client_authz_policy_client_spec.rb
+++ spec/client/client_authz_policy_client_spec.rb
@@ -94,4 +94,27 @@ RSpec.describe KeycloakAdmin::ClientAuthzPolicyClient do
     end
   end
 
+  describe '#find_by' do
+    let(:realm_name) { "valid-realm" }
+    let(:client_id) { "valid-client-id" }
+    let(:type) { :role }
+    let(:name) { "policy name" }
+    before(:each) do
+      @client_authz_policy = KeycloakAdmin.realm(realm_name).authz_policies(client_id, type)
+      stub_token_client
+      allow_any_instance_of(RestClient::Resource).to receive(:get).and_return '[{"id":"234f6f33-ef03-4f3f-a8c0-ad7bca27b720","name":"policy name","description":"policy description","type":"role","logic":"POSITIVE","decisionStrategy":"UNANIMOUS","config":{"roles":"[{\"id\":\"1d305dbe-6379-4900-8e63-96541006160a\",\"required\":false}]"}}]'
+    end
+
+    it "returns list of authz policies" do
+      response = @client_authz_policy.find_by(client_id, name, type)
+      expect(response.size).to eq 1
+      expect(response[0].id).to eq "234f6f33-ef03-4f3f-a8c0-ad7bca27b720"
+      expect(response[0].name).to eq "policy name"
+      expect(response[0].type).to eq "role"
+      expect(response[0].logic).to eq "POSITIVE"
+      expect(response[0].decision_strategy).to eq "UNANIMOUS"
+      expect(response[0].config.roles[0].id).to eq "1d305dbe-6379-4900-8e63-96541006160a"
+    end
+  end
+
 end
