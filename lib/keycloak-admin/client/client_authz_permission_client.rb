a2f42592a3269225894a49b772f2fe31b2b536af
Mohammed O. Tillawy <tillawy@gmail.com>
2024-06-29 14:46:41 +0300

    authz_permission_permission_client

--- lib/keycloak-admin/client/client_authz_permission_client.rb
+++ lib/keycloak-admin/client/client_authz_permission_client.rb
@@ -1,2 +1,63 @@
-# frozen_string_literal: true
+module KeycloakAdmin
+  class ClientAuthzPermissionClient < Client
+    def initialize(configuration, realm_client, client_id, type)
+      super(configuration)
+      raise ArgumentError.new("realm must be defined") unless realm_client.name_defined?
+      raise ArgumentError.new("bad permission type") unless %i[resource scope].include? type.to_sym
 
+      @realm_client = realm_client
+      @client_id = client_id
+      @type = type
+    end
+
+    def delete(permission_id)
+      execute_http do
+        RestClient::Resource.new(authz_permission_url(@client_id, permission_id), @configuration.rest_client_options).delete(headers)
+      end
+      true
+    end
+
+    def create!(name, description, decision_strategy,logic = "POSITIVE", resources = [], policies = [], scopes = [], resource_type = nil)
+      response = save(build(name, description, decision_strategy, logic, resources, policies, scopes, resource_type))
+      ClientAuthzPermissionRepresentation.from_hash(JSON.parse(response))
+    end
+
+    def save(permission_representation)
+      execute_http do
+        RestClient::Resource.new(authz_permission_url(@client_id), @configuration.rest_client_options).post(
+          create_payload(permission_representation), headers
+        )
+      end
+    end
+
+    def list
+      response = execute_http do
+        RestClient::Resource.new(authz_permission_url(@client_id), @configuration.rest_client_options).get(headers)
+      end
+      JSON.parse(response).map { |role_as_hash| ClientAuthzPermissionRepresentation.from_hash(role_as_hash) }
+    end
+
+    def authz_permission_url(client_id, id = nil)
+      if id
+        "#{@realm_client.realm_admin_url}/clients/#{client_id}/authz/resource-server/permission/resource/#{@type}/#{id}"
+      else
+        "#{@realm_client.realm_admin_url}/clients/#{client_id}/authz/resource-server/permission/#{@type}"
+      end
+    end
+
+    def build(name, description, decision_strategy, logic, resources, policies, scopes, resource_type)
+      policy                   = ClientAuthzPermissionRepresentation.new
+      policy.name              = name
+      policy.description       = description
+      policy.type              = @type
+      policy.decision_strategy = decision_strategy
+      policy.resource_type     = resource_type
+      policy.resources         = resources
+      policy.policies          = policies
+      policy.scopes            = scopes
+      policy.logic             = logic
+      policy
+    end
+
+  end
+end
--- spec/client/client_authz_permission_client_spec.rb
+++ spec/client/client_authz_permission_client_spec.rb
@@ -1,2 +1,62 @@
-# frozen_string_literal: true
+RSpec.describe KeycloakAdmin::ClientAuthzPermissionClient do
+  describe '#authz_permission_url' do
+    let(:realm_name) { "valid-realm" }
+    let(:client_id) { "valid-client-id" }
+    before(:each) do
+      @client_authz_permission = KeycloakAdmin.realm(realm_name).authz_permissions(client_id, "resource")
+    end
 
+    it "return a proper url" do
+      expect(@client_authz_permission.authz_permission_url(client_id)).to eq "http://auth.service.io/auth/admin/realms/valid-realm/clients/valid-client-id/authz/resource-server/permission/resource"
+    end
+  end
+
+  describe '#delete' do
+    let(:realm_name) { "valid-realm" }
+    let(:client_id) { "valid-client-id" }
+    before(:each) do
+      @client_authz_permission = KeycloakAdmin.realm(realm_name).authz_permissions(client_id, "resource")
+      stub_token_client
+      allow_any_instance_of(RestClient::Resource).to receive(:delete).and_return 'true'
+    end
+
+    it "deletes a permission" do
+      expect(@client_authz_permission.delete("valid-permission-id")).to eq true
+    end
+  end
+
+  describe '#authz_permission_url' do
+    let(:realm_name) { "valid-realm" }
+    let(:client_id) { "valid-client-id" }
+    before(:each) do
+      @client_authz_permission = KeycloakAdmin.realm(realm_name).authz_permissions(client_id, "resource")
+    end
+
+    it "return a proper url" do
+      expect(@client_authz_permission.authz_permission_url(client_id)).to eq "http://auth.service.io/auth/admin/realms/valid-realm/clients/valid-client-id/authz/resource-server/permission/resource"
+    end
+  end
+
+  describe '#list' do
+    let(:realm_name) { "valid-realm" }
+    let(:client_id) { "valid-client-id" }
+    before(:each) do
+      @client_authz_permission = KeycloakAdmin.realm(realm_name).authz_permissions(client_id, "resource")
+      stub_token_client
+      allow_any_instance_of(RestClient::Resource).to receive(:get).and_return '[{"id":"245ce612-ccdc-4426-8ea7-e0e29a718033","name":"Default Permission","description":"A permission that applies to the default resource type","type":"resource","logic":"POSITIVE","decisionStrategy":"UNANIMOUS","resourceType":"urn:dummy-client:resources:default"},{"id":"06a21e38-4e92-466d-8647-ffcd9c7b51c3","name":"delme policy","description":"delme polidy ","type":"resource","logic":"POSITIVE","decisionStrategy":"UNANIMOUS","resourceType":"asdfasdf"}]'
+
+    end
+
+    it "returns list of authz permissions" do
+      response = @client_authz_permission.list
+      expect(response.size).to eq 2
+      expect(response[0].id).to eq "245ce612-ccdc-4426-8ea7-e0e29a718033"
+      expect(response[0].name).to eq "Default Permission"
+      expect(response[0].description).to eq "A permission that applies to the default resource type"
+      expect(response[0].logic).to eq "POSITIVE"
+      expect(response[0].decision_strategy).to eq "UNANIMOUS"
+      expect(response[0].resource_type).to eq "urn:dummy-client:resources:default"
+    end
+  end
+
+end
