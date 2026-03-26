43868f6c1cf84dc5cf5c429f5dcabbc9f30ec0cf
Aaron Delate <aaron@zero-one.io>
2026-03-26 13:43:48 +0200

    revert: casing of protocolMappers

--- lib/keycloak-admin/representation/client_scope_representation.rb
+++ lib/keycloak-admin/representation/client_scope_representation.rb
@@ -8,13 +8,13 @@ module KeycloakAdmin
                   :protocolMappers
 
     def self.from_hash(hash)
-      rep                  = new
-      rep.id               = hash["id"]
-      rep.name             = hash["name"]
-      rep.description      = hash["description"]
-      rep.protocol         = hash["protocol"]
-      rep.attributes       = hash["attributes"]
-      rep.protocol_mappers = (hash["protocolMappers"] || []).map { |m| ProtocolMapperRepresentation.from_hash(m) }
+      rep                 = new
+      rep.id              = hash["id"]
+      rep.name            = hash["name"]
+      rep.description     = hash["description"]
+      rep.protocol        = hash["protocol"]
+      rep.attributes      = hash["attributes"]
+      rep.protocolMappers = (hash["protocolMappers"] || []).map { |m| ProtocolMapperRepresentation.from_hash(m) }
       rep
     end
   end
